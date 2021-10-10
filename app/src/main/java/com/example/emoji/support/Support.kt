0af338d3561de9cfd987bd5882917f85e1b8f2c1
Yana Glad <55493845+YanaGlad@users.noreply.github.com>
2021-10-10 14:12:47 +0300

    fix names and no unit tests

--- app/src/main/java/com/example/emoji/support/Support.kt
+++ app/src/main/java/com/example/emoji/support/Support.kt
@@ -1,11 +1,8 @@
 package com.example.emoji.support
 
-import android.content.res.Resources
 import android.graphics.Rect
 import android.view.View
 import android.view.ViewGroup
-import androidx.annotation.Px
-import kotlin.math.roundToInt
 
 
 fun View.width(): Int {
@@ -20,13 +17,13 @@ fun View.height(): Int {
 
 fun View.layout(rect: Rect) = layout(rect.left, rect.top, rect.right, rect.bottom)
 
-fun View.rect(rect: Rect, l: Int, t: Int): Rect {
+fun View.rect(rect: Rect, _left: Int, _top: Int): Rect {
     val viewLayoutParams: ViewGroup.MarginLayoutParams =
         this.layoutParams as ViewGroup.MarginLayoutParams
 
     rect.apply {
-        left = l + viewLayoutParams.leftMargin
-        top = t + viewLayoutParams.topMargin
+        left = _left + viewLayoutParams.leftMargin
+        top = _top + viewLayoutParams.topMargin
         right = rect.left + measuredWidth + viewLayoutParams.rightMargin
         bottom = rect.top + measuredHeight + viewLayoutParams.bottomMargin
     }
@@ -35,16 +32,16 @@ fun View.rect(rect: Rect, l: Int, t: Int): Rect {
 
 fun View.rect(
     rect: Rect,
-    leftBorder: Int,
-    topBorder: Int,
+    _left: Int,
+    _top: Int,
     rightBorder: Int
 ): Rect {
     val viewLayoutParams = this.layoutParams as ViewGroup.MarginLayoutParams
 
     viewLayoutParams.also {
         rect.apply {
-            left = leftBorder + it.leftMargin
-            top = topBorder + it.topMargin
+            left = _left + it.leftMargin
+            top = _top + it.topMargin
             right = rightBorder + it.rightMargin
             bottom = top + measuredHeight + it.bottomMargin
         }
